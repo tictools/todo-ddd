495812526f0c930d098d3806fe0f170fba781846
tictools <jaumevillarreal@gmail.com>
2024-03-18 16:14:01 +0100

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <header className="main-header">
+        <h1>
+          TO
+          <span>DDD</span>O
+        </h1>
+        <h2>TODO app based on DDD approach</h2>
+      </header>
+    </>
+  );
+}
+
+export default App;
