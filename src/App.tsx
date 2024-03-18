f19f10478b4ab87f81e567fb124479e37192a86e
tictools <jaumevillarreal@gmail.com>
2024-03-18 22:24:21 +0100

    feat: add version in header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { version } from "../package.json";
 import styles from "./App.module.css";
 
 function App() {
   return (
     <>
       <header className={styles["main-header"]}>
+        <span className="version">{version}</span>
         <h1>
           TO
           <span>DDD</span>O
