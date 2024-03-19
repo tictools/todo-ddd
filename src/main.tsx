e1b9cbf1bb1d699fe03c1715a6d2060c660bbf37
tictools <jaumevillarreal@gmail.com>
2024-03-19 09:44:55 +0100

    feat: test lint-staged

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-const foo = 1;
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
